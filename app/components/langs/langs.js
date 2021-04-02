d2f3a82f73f2d3d846d9fc98c4640ab4fbfa5e94
Anastasia Fefilova <allblue312@gmail.com>
2021-04-02 18:06:36 +0300

    feat(langs): добавлена функция editLang, передающая новое название языка

--- app/components/langs/langs.js
+++ app/components/langs/langs.js
@@ -7,34 +7,36 @@ class Language {
 
     $(window).on('main:ready', (e, data) => {
 
-      const $langs_item =  this.$element.find(".langs__item")
+      const $langsItems = this.$element.find(".langs__items");
+      const languages = Object.keys(data.texts[0]);
+      const $dropdown = this.$element.find('p')
 
-      data.texts.forEach(element => {
-        const $langItem = $(`<li class="langs__item">${element}</li>`)
-        $langItem.insertBefore($langs_item)
+      languages.forEach(key => {
+        const $langsItem = $(`<li class="langs__item" data-key="${key}">${key}</li>`)
+        $langsItems.append($langsItem)
+        $langsItem.on("click", this.editLang)
       });
 
-      const $dropdown = this.$element.find('p')
       $dropdown.on("click", this.onClickDropDown);
+
     })
   }
 
   onClickDropDown = (e) => {
-    console.log(e)
-    const $items = this.$element.find('.langs__items')
-    $items.addClass("langs_active")
-
+    const $items = this.$element.find('.langs')
+    const isActive = $items.hasClass("langs_active")
+    $items.toggleClass("langs_active", !isActive)
   }
 
-  editLang = () => {
-
+  editLang = (e) => {
+    const $items = this.$element.find('.langs')
+   //$items.data("key")
+    console.log($(e.currentTarget).text())
+    $('.langs p').text($(e.currentTarget).text())
+    $items.toggleClass("langs_active", false)
   }
 }
 
-
-$('.langs').each((index, element) => {
+$('.header__right').each((index, element) => {
   new Language($(element));
 });
-
-
-
